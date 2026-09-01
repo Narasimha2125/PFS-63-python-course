e64c65b8cf998fb198aafc7078455c7ce5a565f6
Narasimha2125 <narsimhareddybaireddy2004@gmail.com>
2026-09-01 16:34:15 +0530

    data types

---
+++ Day-3/convert data types.py
@@ -0,0 +1,76 @@
+
+a = 10
+int(a)
+10
+float(a)
+10.0
+complex(a)
+(10+0j)
+bool(a)
+True
+float = 10.0
+float = 10.0
+a = 10.0
+int(a)
+10
+complex(a)
+(10+0j)
+a = 10+3j
+bool(a)
+True
+str(a)
+'(10+3j)'
+list = [1, 2, 3, 4]
+int(list)
+
+complex(list)
+str(list)
+'[1, 2, 3, 4]'
+tuple(list)
+(1, 2, 3, 4)
+set(list)
+{1, 2, 3, 4}
+bool(a)
+True
+b = {1, 2, 3, 4}
+int(b)
+
+complex(b)
+list(b)
+str(b)
+'{1, 2, 3, 4}'
+set(b)
+{1, 2, 3, 4}
+bool(b)
+True
+a = (1, 2, 3, 4)
+int(a)
+float(a)
+complex(a)
+set(a)
+{1, 2, 3, 4}
+list(a)
+str(a)
+'(1, 2, 3, 4)'
+bool(a)
+True
+str = '10'
+int(a)
+int(str)
+10
+float(a)
+complex(a)
+float(str)
+complex(str)
+(10+0j)
+list(str)
+tuple(str)
+('1', '0')
+set(str)
+{'0', '1'}
+bool(str)
+True
+a = true
+a = 'true'
+int(a)
+
---
+++ Day-3/datatyes of python.py
@@ -0,0 +1,39 @@
+
+count = 10
+count =7
+count
+7
+price = 99.00
+price
+99.0
+c =  3+  6j
+c
+(3+6j)
+type(c)
+s =  "codegnan"
+s = 'code'
+type(s)
+s = [1, 2, 3, 4, fvfdfxc, 34, 23,]
+s = [1, 2, 3, 4]
+s
+[1, 2, 3, 4]
+type(s)
+s = (1, 2, 3, 4, )
+s
+(1, 2, 3, 4)
+type(s)
+s = {1, 2, 3, 4, 5,}
+s
+{1, 2, 3, 4, 5}
+type(s)
+s = ture
+s = ture
+s = 'true'
+type(s)
+s = (1, 2, 3, 4)
+
+s = (1,2,3)
+s.add(4)
+s.add(4)
+s = {1, 2, 3, 4}
+
