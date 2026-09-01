7708f31a05c000fabe31d2a38b49c5760cdc9388
Narasimha2125 <narsimhareddybaireddy2004@gmail.com>
2026-09-01 16:15:02 +0530

    opreation variable

---
+++ Day-2/operation variable.py
@@ -0,0 +1,29 @@
+import keyword
+
+print(keyword.kwlist) #List of all keywords
+print(len(keyword.kwlist)) #Total number of keywords
+
+a =  10;
+A = 20
+a
+10
+A
+20
+a=b=c = 10
+a
+10
+A = 20
+a
+10
+a,b,c = 10,20,30
+a
+10
+b
+20
+c
+30
+a,b = b,c
+a
+20
+b
+30
