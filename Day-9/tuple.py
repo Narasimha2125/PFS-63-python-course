b1ae89c81a43f06c29648eefe9535a77df785381
Narasimha2125 <narsimhareddybaireddy2004@gmail.com>
2026-09-02 09:43:10 +0530

    Tuple

---
+++ Day-9/tuple.py
@@ -0,0 +1,63 @@
+# Python 3.11.0 (main, Oct 24 2022, 18:26:48) [MSC v.1933 64 bit (AMD64)] on win32
+# Type "help", "copyright", "credits" or "license()" for more information.
+# print('***TUPLES***')
+# ***TUPLES***
+# t = ()
+# type(t)
+# <class 'tuple'>
+# t = tuple()
+# type(t)
+# <class 'tuple'>
+# t = (10)
+# type(t)
+# <class 'int'>
+# print('TUPLE OPERATIONS:')
+# TUPLE OPERATIONS:
+# a = (1, 2)
+# b = (3, 4)
+# a + b
+# (1, 2, 3, 4)
+# a * 3
+# (1, 2, 1, 2, 1, 2)
+# a[1]
+# 2
+# a[:1]
+# (1,)
+# >>> 2 in a
+# True
+# >>> 3 not in a
+# True
+# >>> len(a)
+# 2
+# >>> max(a)
+# 2
+# >>> min(a)
+# 1
+# >>> sum(a)
+# 3
+# >>> sorted(a)
+# [1, 2]
+# >>> a.count(2)
+# 1
+# >>> a.index(2)
+# 1
+# >>> a = 10, 20, 30
+# >>> a
+# (10, 20, 30)
+# >>> data = (10, 20, 30)
+# >>> a, b, c = data
+# >>> a
+# 10
+# >>> b
+# 20
+# >>> c
+# 30
+# >>> data = ((1, 2), (3, 4))
+# >>> data[1]
+# (3, 4)
+# >>> data[1][0]
+# 3
+# >>> data = (10, [20, 30], 40)
+# >>> data[1].append(50)
+# >>> data
+# (10, [20, 30, 50], 40)
