ec7abea99eb452cd78a60939d69c5e3a47b67b83
Narasimha2125 <narsimhareddybaireddy2004@gmail.com>
2026-09-03 13:45:50 +0530

    For loop

---
+++ Day-12/forloop.py
@@ -0,0 +1,44 @@
+# # s = input("Enter the name: ")
+# # for ch in s:
+# #     if ch in 'aeiouAEIOU':
+# #         print(ch)
+
+# # l = [10, 23, 30, 45, 1, 3, 15, 16, 18, 19, 21]
+# # for i in l:
+# #     if i % 2 == 0:
+# #         print(i, "is Even Number")
+# #     else:
+# #         print(i, "is Odd Number")
+
+# # marks = (90, 20, 35, 46, 78, 92, 87, 48)
+# # for i in marks:
+# #     if i > 35:
+# #         print(i," is Passed")
+# #     else:
+# #         print(i, "is Failed")
+
+# # followers = {'Bharat', 'Avinash', 'Ganesh', 'Lokesh'}
+# # for i in followers:
+# #     print(i)
+
+# # bus = {'s1': 'Booked', 's2': 'Available', 's3': 'Available', 's4': 'Booked', 's5': 'Available'}
+# # for seat in bus:
+# #     if bus.get(seat) == 'Available':
+# #         print(seat)
+
+# # for i in range(1, 11, 1):
+# #     print(i, end=" ")
+
+# # for i in range(10, 0, -1):
+# #     print(i, end=" ")
+
+# # for i in range(1, 100, 2):
+# #     print(i, end=" ")
+
+# # for i in range(5, 51, 5):
+# #     print(i, end=" ")
+
+
+# n = int(input("Enter the table number: "))
+# for i in range(1, 11):
+#     print(str(n) + " x " + str(i) + " = " + str(n*i))
---
+++ Day-12/nestedif.py
@@ -0,0 +1,32 @@
+# # follows_account = eval(input("Follows Account: "))
+# # close_friend = eval(input("Close Friend: "))
+
+# # if follows_account:
+# #     if close_friend:
+# #         print("Story Visible")
+# #     else:
+# #         print("Not in Close Friends List")
+# # else:
+# #     print("Follow the Account First")
+
+# # registered_player = eval(input('Registered Player: '))
+# # fee_paid = eval(input("Fee Paid: "))
+
+# # if registered_player:
+# #     if fee_paid:
+# #         print("Tournament Entry Confirmed")
+# #     else:
+# #         print("Entry Fee Pending")
+# # else:
+# #     print("Registration Required")
+
+# link_active = eval(input("Link Active: "))
+# permission_granted = eval(input("Permission Granted: "))
+
+# if link_active:
+#     if permission_granted:
+#         print("File Opened Successfully")
+#     else:
+#         print("Access Denied")
+# else:
+#     print("Invalid File Link")
