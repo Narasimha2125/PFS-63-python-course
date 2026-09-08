40d43a93d58873dc34532f8ba0fdf7b3af65c774
Narasimha2125 <narsimhareddybaireddy2004@gmail.com>
2026-09-08 09:09:23 +0530

    While loop

---
+++ Day-14/forwithelse.py
@@ -0,0 +1,67 @@
+# # for i in range(1, 10):
+# #     if i == 15:
+# #         break
+# #     print(i)
+
+# # else:
+# #     print('End of the loop')
+
+
+# # pin = 1234
+
+# # for _ in range(5):
+# #     user_pin = int(input("Enter the pin: "))
+
+# #     if pin == user_pin:
+# #         print("Unlock Phone")
+# #         break
+# #     else:
+# #         print('Invalid Pin')
+
+# # else:
+# #     print("Try Again after 30 seconds")
+
+
+
+
+# #Calculate factors of a number program:
+# # n = int(input('Enter the number: '))
+# # print(f'Factors of {n}: ', end = " ")
+# # for i in range(1, n+1):
+# #     if n % i == 0:
+# #         print(i, end = " ")
+
+
+
+# # n = int(input("Enter the number: "))
+# # count = 0
+# # for i in range(1, n+1):
+# #     if n % i == 0:
+# #         count += 1
+# # if count <= 2:
+# #     print("Prime number")
+# # else:
+# #     print("Not a Prime number")
+
+
+# # n = int(input("Enter the number: "))
+# # count = 0
+# # for i in range(2, int(n ** 0.5) + 1):
+# #     if n % i == 0:
+# #         count += 1
+
+# # if count == 0:
+# #     print("Prime number")
+# # else:
+# #     print("Not a Prime number")
+
+
+# n = int(input("Enter the number: "))
+# count = 0
+# for i in range(2, int(n ** 0.5) + 1):
+#     if n % i == 0:
+#         print("Not a Prime Number")
+#         break
+# else:
+#     print("Prime Number")
+
---
+++ Day-14/whileloop.py
@@ -0,0 +1,52 @@
+# # i = 1
+# # while i <= 10:
+# #     print(i)
+# #     i += 1
+
+
+# # i = 10
+# # while i >= 1:
+# #     print(i)
+# #     i -= 1
+
+
+# # i = 2
+# # while i <= 100:
+# #     print(i, end=",")
+# #     i += 2
+
+# # s = 'Codegnan'
+# # i = len(s)-1
+# # while i >= 0:
+# #     print(s[i], end="")
+# #     i -= 1
+
+
+# # l = [1, 2, 3, 0, 5, 0, 3, 0, 6, 7, 0, 8, 8, 0, 1, 0]
+# # while 0 in l:
+# #     l.remove(0)
+# # print(l)
+
+
+
+# # data = {}
+# # total = 0
+# # while True:
+# #     product_name = input("Enter the product name: ")
+# #     if product_name == 'exit':
+# #         break
+# #     price = int(input("Enter the price: "))
+# #     data[product_name] = price
+# #     total += price
+# # print(data)
+# # print(f'Total Bill: {total}')
+
+
+# i = 0
+# while i < 10:
+#     i += 1
+#     if i == 5:
+#         continue
+#     print(i)
+# else:
+#     print("End of the Loop")
