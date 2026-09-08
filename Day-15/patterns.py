d7e88774b7b19eae108b33525515d1d045f301e4
Narasimha2125 <narsimhareddybaireddy2004@gmail.com>
2026-09-08 09:10:10 +0530

    Patterns

---
+++ Day-15/patterns.py
@@ -0,0 +1,47 @@
+# for i in range(5):
+#     for j in range(10):
+#         print("*", end = "")
+#     print()
+
+
+# for i in range(6,1,-1):
+#     for j in range(i):
+#         print('*', end = '')
+#     print()
+
+
+# for i in range(5):
+#     for j in range(5):
+#         print(i, end = '')
+#     print()
+
+
+# for i in range(5):
+#     for j in range(5):
+#         print(i+j, end = '')
+#     print()
+
+
+# for i in range(5):
+#     for j in range(5):
+#         print(i%2, end = '')
+#     print()
+
+
+# for i in range(5):
+#     for j in range(5):
+#         print((i+j) %2, end = '')
+#     print()
+
+
+# for i in range(5):
+#     for j in range(i+1):
+#         print('*', end='')
+#     print()
+
+# for i in range(5):
+#     for j in range(5-i):
+#         print('*', end = '')
+#     print()
+
+
