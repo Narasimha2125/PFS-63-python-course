1471012341a9390857a2dcab51edae3be5d8add7
Narasimha2125 <narsimhareddybaireddy2004@gmail.com>
2026-09-10 11:20:29 +0530

    Buildin

---
+++ Day-25/buildin.py
@@ -0,0 +1,78 @@
+# import sys
+# # print(sys.path)
+# # print(sys.version)
+# print("start")
+# sys.exit()
+# print("end")
+
+# import platform
+# print(platform.system())
+# print(platform.release())
+# print(platform.processor())
+
+# Function         #Description
+# math.sqrt(x) -   Returns the square root of x
+# math.pow(x, y) - x raised to the power y (x^y)
+# math.ceil(x) -   Smallest integer ≥ x
+# math.floor(x) -  Largest integer ≤ x
+# math.fabs(x) -   Absolute value of x
+# math.factorial(x) -   Factorial of x (x!)
+# math.gcd(x, y) -    Greatest common divisor
+# math.log(x, base) - Logarithm of x to the given base
+# math.sin(x) -    Sine of x (x in radians)
+# math.cos(x) -    Cosine of x
+# math.tan(x) -    Tangent of x
+# math.degrees(x) - Convert radians to degrees
+# math.radians(x) - Convert degrees to radians
+
+# import math
+# print(math.pi)
+# print(math.e)
+
+# print(math.sqrt(36))
+# print(math.pow(2,3))
+
+# print(math.ceil(12.00001))
+# print(math.ceil(12.3))
+# print(math.ceil(12.6))
+# print(math.ceil(12.9999999))
+
+# print(math.floor(12.00001))
+# print(math.floor(12.3))
+# print(math.ceil(12.5))
+# print(math.floor(12.999999))
+
+# import math
+# print(math.fabs(-10))
+# print(math.factorial(5))
+# print(math.gcd(8,24))
+# print(math.log(2,2))
+# print(math.sin(30))
+# print(math.cos(30))
+# print(math.tan(45))
+# print(math.degrees(30))
+# print(math.radians(30))
+
+# Function               Description
+# random.random()         Returns a float in the range [0.0, 1.0)
+# random.randint(a, b)    Returns random integer between a and b (inclusive)
+# random.uniform(a, b)    Returns a float between a and b
+# random.choice(seq)      Returns a random element from a non-empty sequence
+# random.choices(seq,k=n) Returns a list of k random elements from seq
+# random.shuffle(list)    Shuffles the list in place
+# random.seed(n)          Sets the seed for reproducibility
+# import random
+# #random.seed(10)
+# print(random.randint(1,10))
+# print(random.randint(100000,9999999))
+# print(random.random())
+# print(random.uniform(1,16))
+
+# l= ['R','P','S']
+# print(random.choice(l))
+# print(random.choices(l,k=2))
+
+# random.shuffle(l)
+# print(l)
+
+
