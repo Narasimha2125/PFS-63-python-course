aaf7312ef0a714dbdc0a6f6deb0b5d254fa19b24
Narasimha2125 <narsimhareddybaireddy2004@gmail.com>
2026-09-10 11:28:55 +0530

    Regex

---
+++ Day-34/regex.py
@@ -0,0 +1,50 @@
+# MATCH
+# import re
+# pattern = r'[]'
+# text = 'codegnan'
+# res = re.match(pattern,text)
+# print(res.group() if res else "Pattern not found")
+
+#SEARCH
+# import re
+# pattern = r'code'
+# text = 'codegnan2026d'
+# res = re.search(pattern,text)
+# print(res.group() if res else "Pattern not found")
+
+# # FINDALL
+# import re
+# pattern = r'[0-9]'
+# text = 'codegnan2026 python version 3.14'
+# res = re.findall(pattern,text)
+# print(res)
+# print(res.group() if res else "Pattern not found")
+
+#FINDITER
+# import re
+# pattern = r'[0-9]'
+# text = 'codegnan2026d'
+# res = re.finditer(pattern,text)
+# for i in res:
+#     print(i.group(),i.start())
+
+# # FULL MATCH
+# import re
+# pattern = r'[0-9]{10}'
+# text = '9876543210'
+# res = re,fullmatch(pattern,text)
+# print(res)
+
+# SPLIT
+# import re
+# pattern = r'[ ,]+'
+# text = 'codegnan,2026,python'
+# res = re.split(pattern,text)
+# print(res)
+
+# SUB
+# import re
+# pattern = r'[0-9]'
+# text = 'codegnan2026d'
+# res = re.sub(pattern,'X',text)
+# print(res)
